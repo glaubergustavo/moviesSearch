6cca8d020a42d8b748ec81da5f623d394cf13220
Glauber Gustavo <gustavo.glauber@gmail.com>
2023-01-07 12:18:20 -0300

    Initial Commit

---
+++ moviesSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  moviesSearch
+//
+//  Created by Glauber Gustavo on 07/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
